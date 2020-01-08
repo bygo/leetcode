6e722941d44e1171cbd2346a6ac8d3a02e2b8000
by <temporarycrash@gmail.com>
2020-01-08 23:51:41 +0800

    1 and 2

---
+++ helper.go
@@ -0,0 +1,101 @@
+package leetcode
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"strconv"
+	"time"
+)
+
+var n time.Time
+var memStart runtime.MemStats
+var memEnd runtime.MemStats
+
+func Start() {
+	n = time.Now()
+	runtime.ReadMemStats(&memStart)
+}
+
+func End() {
+	timeCounter()
+	memCounter()
+}
+
+func D(callback func() interface{}) {
+	Start()
+	result := callback()
+	End()
+	println()
+	display("", reflect.ValueOf(result))
+}
+
+func timeCounter() {
+	println("--------Result-----------")
+	println("runtime: " + time.Since(n).String())
+}
+
+func memCounter() {
+	runtime.ReadMemStats(&memEnd)
+	MemString := strconv.Itoa(int(memEnd.Alloc - memStart.Alloc))
+	println(" memory: " + MemString + " B")
+}
+
+func display(path string, v reflect.Value) {
+	switch v.Kind() {
+	case reflect.Invalid:
+		fmt.Printf("%s = invalid\n", path)
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < v.Len(); i++ {
+			display(fmt.Sprintf("%s[%d]", path, i), v.Index(i))
+		}
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
+			fieldPath := fmt.Sprintf("%s.%s", path, v.Type().Field(i).Name)
+			display(fieldPath, v.Field(i))
+		}
+	case reflect.Map:
+		for _, key := range v.MapKeys() {
+			display(fmt.Sprintf("%s[%s]", path,
+				formatAtom(key)), v.MapIndex(key))
+		}
+	case reflect.Ptr:
+		if v.IsNil() {
+			fmt.Printf("%s = nil\n", path)
+		} else {
+			display(fmt.Sprintf("(*%s)", path), v.Elem())
+		}
+	case reflect.Interface:
+		if v.IsNil() {
+			fmt.Printf("%s = nil\n", path)
+		} else {
+			fmt.Printf("%s.type = %s\n", path, v.Elem().Type())
+			display(path+".value", v.Elem())
+		}
+	default: // basic types, channels, funcs
+		fmt.Printf("%s = %s\n", path, formatAtom(v))
+	}
+}
+
+func formatAtom(v reflect.Value) string {
+	switch v.Kind() {
+	case reflect.Invalid:
+		return "invalid"
+	case reflect.Int, reflect.Int8, reflect.Int16,
+		reflect.Int32, reflect.Int64:
+		return strconv.FormatInt(v.Int(), 10)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return strconv.FormatUint(v.Uint(), 10)
+		// ...floating-point and complex cases omitted for brevity...
+	case reflect.Bool:
+		return strconv.FormatBool(v.Bool())
+	case reflect.String:
+		return strconv.Quote(v.String())
+	case reflect.Chan, reflect.Func, reflect.Ptr, reflect.Slice, reflect.Map:
+		return v.Type().String() + " 0x" +
+			strconv.FormatUint(uint64(v.Pointer()), 16)
+	default: // reflect.Array, reflect.Struct, reflect.Interface
+		return v.Type().String() + " value"
+	}
+}
