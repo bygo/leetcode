8d76237ca5e5a95396616cb185394172e45f6566
by <temporarycrash@gmail.com>
2020-01-30 14:35:03 +0800

    replace `multiple`  with 32bit

--- 0029.divide/main.go
+++ 0029.divide/main.go
@@ -10,28 +10,36 @@ func divide(dividend int, divisor int) int {
 	if divisor < 0 {
 		divisor = -divisor
 	}
-	var multiple int
+	var multiple int32
 	var aMultiple = divisor
 	for divisor <= dividend {
 		d := aMultiple
-		m := 1
+		m := int32(1)
 		for divisor+d<<1 < dividend {
 			d <<= 1
 			m <<= 1
 		}
 		multiple += m
+
 		divisor += d
+	}
+	if multiple < 0 { //处理溢出
+		if negative {
+			return math.MinInt32
+		} else {
+			return math.MaxInt32
+		}
 	}
 	if negative {
 		multiple = -multiple
 	}
 	if multiple > math.MaxInt32 {
-		multiple = math.MaxInt32
+		return math.MaxInt32
 	}
 	if multiple < math.MinInt32 {
-		multiple = math.MinInt32
+		return math.MinInt32
 	}
-	return multiple
+	return int(multiple)
 }
 
 /**
