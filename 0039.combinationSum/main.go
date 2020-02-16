75b5000993162a3869a234f2f878d2fb54bba9a2
by <temporarycrash@gmail.com>
2020-02-16 13:23:55 +0800

    no message

---
+++ 0039.combinationSum/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"sort"
+)
+
+func combinationSum(candidates []int, target int) [][]int {
+	sort.Ints(candidates) //快排，懒得写
+	res := [][]int{}
+	dfs(candidates, nil, target, 0, &res) //深度优先
+	return res
+}
+
+func dfs(candidates, nums []int, target, left int, res *[][]int) {
+	if target == 0 { //结算
+		tmp := make([]int, len(nums))
+		copy(tmp, nums)
+		*res = append(*res, tmp)
+		return
+	}
+
+	for i := left; i < len(candidates); i++ { // left限定，形成分支
+		if target < candidates[i] { //剪枝
+			return
+		}
+		dfs(candidates, append(nums, candidates[i]), target-candidates[i], i, res) //分支
+	}
+}
