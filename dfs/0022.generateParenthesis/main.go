f53d48923b10dee3bda54875454f15777cb006e0
by <temporarycrash@gmail.com>
2020-02-17 18:45:59 +0800

    classify

--- dfs/0022.generateParenthesis/main.go
+++ dfs/0022.generateParenthesis/main.go
@@ -1,20 +1,6 @@
 package main
 
-/**
-给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
-
-例如，给出 n = 3，生成结果为：
-
-[
-  "((()))",
-  "(()())",
-  "(())()",
-  "()(())",
-  "()()()"
-]
- */
-
-//深度搜索
+//dfs
 func generateParenthesis(n int) []string {
 	res := make([]string, 0)
 	if n < 1 {
@@ -39,11 +25,6 @@ func dfs(str string, left int, right int, res *[]string) {
 	if right > 0 {
 		dfs(str+")", left, right-1, res) //右树遍历
 	}
-}
-
-//广度搜索
-func generateParenthesis2(n int) []string {
-	return nil
 }
 
 //闭合数
@@ -73,8 +54,3 @@ func closed(n int) []string {
 	}
 	return res
 }
-
-/**
-思路1：深度优先搜索
-思路2：类似17题
- */
--- hash_table/0003.lengthOfLongestSubstring/main.go
+++ hash_table/0003.lengthOfLongestSubstring/main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"leetcode"
-)
-
 /**
 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
 
@@ -61,16 +57,6 @@ func lengthOfLongestSubstring2(s string) int {
 		}
 	}
 	return length
-}
-
-func main() {
-	leetcode.D(func() interface{} {
-		return lengthOfLongestSubstring1("abc")
-	})
-
-	leetcode.D(func() interface{} {
-		return lengthOfLongestSubstring2("abc")
-	})
 }
 
 /**
