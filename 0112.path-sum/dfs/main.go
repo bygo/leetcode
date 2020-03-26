ceadca31b6a707afc0eb1a150c2d10161ef8a75c
by <temporarycrash@gmail.com>
2020-03-26 21:33:31 +0800

    112

---
+++ 0112.path-sum/dfs/main.go
@@ -0,0 +1,17 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil {
+		return sum == root.Val
+	}
+	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
+}
