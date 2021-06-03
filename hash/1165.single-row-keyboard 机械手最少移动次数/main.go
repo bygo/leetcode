f2f27d72dc4e422608fa74a1f4843caef213f62e
by <temporarycrash@gmail.com>
2021-06-03 19:29:09 +0800

    fix

---
+++ hash/1165.single-row-keyboard 机械手最少移动次数/main.go
@@ -0,0 +1,28 @@
+package main
+
+//Link: https://leetcode-cn.com/problems/single-row-keyboard
+
+func calculateTime(keyboard string, word string) int {
+	var m = map[byte]int{}
+	for i := range keyboard {
+		m[keyboard[i]] = i
+	}
+
+	var res = m[word[0]]
+	var l = len(word)
+	var i = 1
+	for i < l {
+		res += abs(m[word[i-1]], m[word[i]])
+		i++
+	}
+
+	return res
+}
+
+func abs(a, b int) int {
+	if a < b {
+		return b - a
+	} else {
+		return a - b
+	}
+}
