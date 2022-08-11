077191003877408a982edcad4032e547853e0a69
bygo <temporarycrash@gmail.com>
2022-08-11 21:45:21 +0800

    update

--- LinkedList/1669.merge-in-between-linked-lists/main.go
+++ LinkedList/1669.merge-in-between-linked-lists/main.go
@@ -1,33 +1,31 @@
 package main
 
-// https://leetcode.cn/problems/merge-in-between-linked-lists
-
-// ❓ 合并两个链表
-
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
+// https://leetcode.cn/problems/merge-in-between-linked-lists
+
 func mergeInBetween(list1 *ListNode, a int, b int, list2 *ListNode) *ListNode {
-	var headLeft = list1
-	b = b - a + 2 // 下一个
+	var res = list1
+	b -= a + 2 // 从0 开始，并且是下一个
 	for 1 < a {
 		list1 = list1.Next
 		a--
 	}
-	tailLeft := list1
+	l := list1
 
 	for 0 < b {
 		list1 = list1.Next
 		b--
 	}
-	rightHead := list1
+	r := list1
 
-	tailLeft.Next = list2
+	l.Next = list2
 	for list2.Next != nil {
 		list2 = list2.Next
 	}
-	list2.Next = rightHead
-	return headLeft
+	list2.Next = r
+	return res
 }
